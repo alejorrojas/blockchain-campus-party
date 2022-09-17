5fc5b6c732d7664a8b77f813c8f83d456430e7b0
Alejo <alejoivanrojas@gmail.com>
2022-09-17 20:36:24 -0300

    move to vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { ethers } from "ethers"
+import contractABI from "../contracts/contract_abi.json"
+import {contractAddress} from "../contracts/data"
+
+
+function App() {
+  
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
