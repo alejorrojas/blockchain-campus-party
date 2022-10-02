6331c03f87998b65ba1cd0e05a7a60c654819b5a
Alejo <alejoivanrojas@gmail.com>
2022-10-02 19:45:21 -0300

    fix utils

--- contracts/utils.js
+++ contracts/utils.js
@@ -1,2 +1,1 @@
-import contractABI from "./contract_abi.json"
-const contractAddress = "0x0B2f7BBbb52a237aFd955A261760A20815aEf839"
+export const contractAddress = "0x0B2f7BBbb52a237aFd955A261760A20815aEf839"
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import { ethers } from "ethers"
 import { contractAddress } from "../contracts/utils"
-import { contractABI } from "../contracts/contract_abi.json"
+import  contractABI  from "../contracts/contract_abi.json"
+
+
 
 
 function App() {
   
   return (
     <div className="App">
-      
+      <img src='/token.png'/>
+      <img src='/react.svg'/>
     </div>
   )
 }
