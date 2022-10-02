c8694d864c982df4cd474e678374785e08d447ab
Alejo <alejoivanrojas@gmail.com>
2022-10-02 19:33:41 -0300

    fix utils

--- contracts/utils.js
+++ contracts/utils.js
@@ -1,6 +1,2 @@
-import { ethers } from "ethers"
 import contractABI from "./contract_abi.json"
 const contractAddress = "0x0B2f7BBbb52a237aFd955A261760A20815aEf839"
-const provider =  new ethers.providers.Web3Provider(window.ethereum)
-const signer = provider.getSigner()
-export const contract = new ethers.Contract(contractAddress, contractABI, signer)
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ethers } from "ethers"
-import {contract} from "../contracts/utils"
+import { contractAddress } from "../contracts/utils"
+import { contractABI } from "../contracts/contract_abi.json"
 
 
 function App() {
